1f124166be80d95095e8b2cc48f1549b7b085042
romanENI <roman.sueur2022@campus-eni.fr>
2025-07-07 17:09:14 +0200

    Initial commit

---
+++ backend/src/test/java/com/quackinduckstries/projectname/ProjectNameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quackinduckstries.projectname;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectNameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
