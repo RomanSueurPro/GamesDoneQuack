7021d423394e09a9460177eeac29e8f80ffad73d
RomanSueurPro <roman.sueur@gmail.com>
2025-07-10 14:53:38 +0200

    Cors error fixed for demonstration

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'frontend';
+  response: string = 'empty';
+
+  constructor(private http: HttpClient){}
+
+    ngOnInit(): void{
+      this.http.get('http://localhost:8080/home', { responseType: 'text'}).subscribe(
+        data => {
+          this.response = data;
+        }
+      );
+    }
+
 }
