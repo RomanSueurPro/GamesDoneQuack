f52bb6cf3359cb38875c8cfc81b251b8850a23ad
RomanSueurPro <roman.sueur@gmail.com>
2025-07-15 14:46:03 +0200

    API test

---
+++ backend/src/main/java/com/quackinduckstries/projectname/controllers/SteamController.java
@@ -0,0 +1,40 @@
+package com.quackinduckstries.projectname.controllers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class SteamController {
+
+	
+	@GetMapping("/steam")
+	public ResponseEntity<String> getSteamData() throws MalformedURLException, IOException{
+		String url = "https://kaamelott.chaudie.re/api/all";
+		
+		HttpURLConnection conn = (HttpURLConnection) new URL (url).openConnection();
+		conn.setRequestMethod("GET");
+		
+		try (BufferedReader reader = new BufferedReader(
+				new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8))){
+			
+			String response = reader.lines().collect(Collectors.joining());
+			return ResponseEntity.ok(response);
+		}
+	}
+	
+	
+	
+	
+}
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,21 +2,29 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-
+import { SteamService } from './steam.service';
+import { NgIf } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule],
+  imports: [RouterOutlet, HttpClientModule, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
+
+
 export class AppComponent {
   title = 'frontend';
   response: string = 'empty';
 
-  constructor(private http: HttpClient){}
+  messages: string[] = [];
+  steamData: string | undefined;
+
+
+  constructor(private http: HttpClient, private steamService: SteamService){}
 
     ngOnInit(): void{
       this.http.get('http://localhost:8080/home', { responseType: 'text'}).subscribe(
@@ -24,6 +32,14 @@ export class AppComponent {
           this.response = data;
         }
       );
+    }
+
+
+    fetchSteamData(){
+      this.steamService.getSteamData().subscribe({
+        next: (data) => this.steamData = JSON.stringify(data),
+        error: (err) => this.steamData = 'Error fetching steam data'
+      });
     }
 
 }
