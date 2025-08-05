1fa0d8fe260c28e003a21122e6bdbdd950ad8c00
RousteauVincent <139780467+RousteauVincent@users.noreply.github.com>
2025-08-05 17:47:43 +0200

    cors error fixing

---
+++ backend/src/main/java/com/quackinduckstries/gamesdonequack/config/SecurityConfig.java
@@ -0,0 +1,71 @@
+package com.quackinduckstries.gamesdonequack.config;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.quackinduckstries.gamesdonequack.GamesDoneQuackApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final GamesDoneQuackApplication gamesDoneQuackApplication;
+
+    SecurityConfig(GamesDoneQuackApplication gamesDoneQuackApplication) {
+        this.gamesDoneQuackApplication = gamesDoneQuackApplication;
+    }
+
+        @Bean
+        SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+                return http
+                                .csrf(csrf -> csrf
+                                                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                                                  .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler()))
+                                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                                .authorizeHttpRequests(
+                                                authorizeHttp -> {
+                                                        authorizeHttp.requestMatchers("/home").permitAll();
+                                                        authorizeHttp.requestMatchers("/favicon.svg").permitAll();
+                                                        authorizeHttp.requestMatchers("/error").permitAll();
+                                                        authorizeHttp.anyRequest().authenticated();
+                                                }
+                                )
+                                .formLogin(l -> l.defaultSuccessUrl("/greeting"))
+                                .logout(l -> l.logoutSuccessUrl("/"))
+                                .build();
+                                
+        }
+        
+        
+        @Bean
+        UrlBasedCorsConfigurationSource corsConfigurationSource() {
+            CorsConfiguration configuration = new CorsConfiguration();
+            configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+            configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+            configuration.setAllowedHeaders(List.of("*"));
+            configuration.setAllowCredentials(true);
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            source.registerCorsConfiguration("/**", configuration);
+            return source;
+        }
+        
+
+        @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user").password("{noop}usertest@12345").authorities("read").build();
+        return new InMemoryUserDetailsManager(user);
+    }
+              
+}
--- backend/src/main/java/com/quackinduckstries/gamesdonequack/controllers/SteamController.java
+++ backend/src/main/java/com/quackinduckstries/gamesdonequack/controllers/SteamController.java
@@ -1,16 +1,9 @@
 package com.quackinduckstries.gamesdonequack.controllers;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.util.stream.Collectors;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,21 +13,6 @@ import org.springframework.web.client.RestClient;
 @RequestMapping("/api")
 public class SteamController {
 
-	
-	@GetMapping("/steam")
-	public ResponseEntity<String> getSteamData() throws MalformedURLException, IOException{
-		String url = "https://kaamelott.chaudie.re/api/all";
-		
-		HttpURLConnection conn = (HttpURLConnection) new URL (url).openConnection();
-		conn.setRequestMethod("GET");
-		
-		try (BufferedReader reader = new BufferedReader(
-				new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8))){
-			
-			String response = reader.lines().collect(Collectors.joining());
-			return ResponseEntity.ok(response);
-		}
-	}
 	
 	
 	@GetMapping("/kaamelott")
