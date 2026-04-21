6febaf93e6640165e6e9381d1c69a16ce0c7325e
RomanSueurPro <roman.sueur@gmail.com>
2026-04-21 18:24:49 +0200

    admin role frontend : permission add / remove works, cancel works.

--- frontend/src/app/pages/admin/role-list/role-list.component.ts
+++ frontend/src/app/pages/admin/role-list/role-list.component.ts
@@ -1,11 +1,9 @@
 import { Component, Input } from '@angular/core';
-import { RoleWithoutPermissions } from '../../../models/RoleWithoutPermissions';
-import { catchError, map, Observable, of } from 'rxjs';
+import { forkJoin } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { RoleAllFields } from '../../../models/RoleAllFields';
 import { MatListModule } from '@angular/material/list';
 import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { PermissionAllFields } from '../../../models/PermissionAllFields';
 import { PermissionWithoutRoles } from '../../../models/PermissionWithoutRoles';
 import { API_ENDPOINTS } from '../../../config/api-endpoints';
 
@@ -21,64 +19,65 @@ export class RoleListComponent {
 
   constructor(private http: HttpClient){
     this.form = new FormGroup({
-      roles: this.rolesControl,
+      rolename: new FormControl(''),
     });
+    
     this.hideSingleSelectionIndicator = false;
+    this.selected = false;
+    this.arrayRoles = [];
   }
-
+  
   @Input()
   hideSingleSelectionIndicator: boolean;
 
+  @Input()
+  selected: boolean;
+
+  selectedRole: any = null;
   form: FormGroup;
-  public arrayRoles: RoleAllFields[] = [];
+  public arrayRoles: RoleAllFields[];
   public arrayPermissions: PermissionWithoutRoles[] = [];
   rolesControl = new FormControl();
+  rolenametextfield: string = "";
+  
 
   public associatedPermissions: PermissionWithoutRoles[] = [];
   public notAssociatedPermissions: PermissionWithoutRoles[] = [];
 
   ngOnInit(){
-    this.loadRoles();
-    this.loadPermissions();
-    
+    this.loadData();
   }
 
-  refreshObject(){
-    this.loadRoles();
+  loadData(){
+    forkJoin({
+      permissions: this.fetchPermissionsObservable(),
+      roles: this.fetchRolesObservable(),
+    }).subscribe({
+      next: ({permissions, roles}) => {
+        this.arrayRoles = roles;
+        this.selectedRole = this.arrayRoles[0] ?? null;
+        this.arrayPermissions = permissions;
+        this.updatePermissionsAssociations(this.selectedRole);
+      },
+      error: () => {
+        console.log('An error occured during component initialization')
+      }
+    });
   }
 
   fetchRolesObservable(){
     return this.http.get<RoleAllFields[]>(API_ENDPOINTS.admin.fetchRoles, {withCredentials: true});
   }
 
-  
-
-  loadRoles(){
-    this.fetchRolesObservable().subscribe({
-      next: (result) => 
-        {
-          this.arrayRoles = result;
-        },
-      error: (error) => console.log(error)
-    })
-  }
-
   fetchPermissionsObservable(){
     return this.http.get<PermissionWithoutRoles[]>(API_ENDPOINTS.admin.fetchPermissions, {withCredentials: true});
   }
 
-  loadPermissions(){
-    this.fetchPermissionsObservable().subscribe({
-      next: (result) => 
-        {
-          this.arrayPermissions = result;
-        },
-      error: (error) => console.log(error)
-    })
-  }
-
-  getPermissionsAssociations(role: RoleAllFields){
+  updatePermissionsAssociations(role: RoleAllFields){
   
+    if(role ===null){
+      return;
+    }
     this.associatedPermissions = [];
     this.notAssociatedPermissions = [];
 
@@ -95,12 +94,40 @@ export class RoleListComponent {
     }
   }
 
-  testWithButton(role: RoleAllFields){
-    console.log(this.arrayRoles);
-    this.getPermissionsAssociations(role);
-    
+  onSelectionChange(event: any) {
+    const selected = event.options[0]?.value;
+    this.selectedRole = selected;
+    this.updatePermissionsAssociations(selected);
   }
-  
+
+  togglePermission(permissionName: string){
+    const permissionObject = this.arrayPermissions.filter((permission) => permission.name === permissionName)[0];
+    
+    if(this.associatedPermissions.includes(permissionObject)){
+      const index = this.associatedPermissions.indexOf(permissionObject, 0);
+      if(index > -1){
+        this.associatedPermissions.splice(index, 1);
+        this.notAssociatedPermissions.push(permissionObject);
+      }
+    }
+    else{
+      const index = this.notAssociatedPermissions.indexOf(permissionObject, 0);
+      if(index > -1){
+        this.notAssociatedPermissions.splice(index, 1);
+        this.associatedPermissions.push(permissionObject);
+      }
+    }
+  }
+
+  cancelChanges(role: RoleAllFields):void{
+    this.updatePermissionsAssociations(role);
+    this.rolenametextfield = role.name;
+  }
+
+  saveChanges(){
+
+  }
+
 }
 
 
